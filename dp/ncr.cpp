51325baba004477cb2b5ef8f582a7f838aaab41e
Rinka Yadav <99800927+Rinka05@users.noreply.github.com>
2023-06-26 03:11:39 +0530

    Create ncr.cpp

---
+++ dp/ncr.cpp
@@ -0,0 +1,20 @@
+
+// question
+// Given two integers n and r, find nCr. Since the answer may be very large, calculate the answer modulo 109+7.
+
+
+class Solution{
+public:
+    int nCr(int n, int r){
+       int const mod = 1e9+7;
+       
+       vector<vector<int>>dp(n+1 , vector<int>(r+1));
+       for(int i = 0 ; i <= n ; i++){
+           for(int j = 0 ; j <= min(i , r) ; j++){
+               if(j==0)dp[i][j] = 1;
+               else dp[i][j] = (dp[i-1][j]%mod +dp[i-1][j-1] % mod)%mod;
+           }
+       }
+       return dp[n][r];
+    }
+};
